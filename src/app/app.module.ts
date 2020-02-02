07ad0d26f829e792f6918902ae9fe00380fd4a12
Oleksandr Bachkai <abachkay@gmail.com>
2020-02-02 19:01:04 +0200

    Removed FirstComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/components/first.component';
 import { ProductComponent } from './product/components/product.component';
 import { ProductListComponent } from './product-list/components/product-list.component';
-import { ProductsService } from './product/services/products.service';
 import { CartComponent } from './cart/components/cart.component';
-import { AveragePipe } from './first/pipes/average.pipe';
 import { CartItemComponent } from './cart-item/components/cart-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
     ProductComponent,
     ProductListComponent,
     CartComponent,
-    AveragePipe,
     CartItemComponent
   ],
   imports: [
